13e8c693496dfd1e7146accdd8cf51a13d72b1ca
Hesam Rezaei <hesamrezaei77@gmail.com>
2021-09-05 23:36:42 +0430

    admin auto creation

--- src/main/java/ir/ac/aut/hesabgar/HesabgarApplication.java
+++ src/main/java/ir/ac/aut/hesabgar/HesabgarApplication.java
@@ -1,6 +1,10 @@
 package ir.ac.aut.hesabgar;
 
+import ir.ac.aut.hesabgar.domain.document.UserInfo;
+import ir.ac.aut.hesabgar.domain.repo.UserInfoRepo;
 import ir.ac.aut.hesabgar.group_feign.GroupGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,12 +13,26 @@ import org.springframework.web.client.HttpServerErrorException;
 @SpringBootApplication
 @EnableFeignClients
 //@EnableFeignClients(basePackageClasses = GroupGateway.class)
-public class HesabgarApplication {
+public class HesabgarApplication implements CommandLineRunner {
+    @Autowired
+    private UserInfoRepo userInfoRepo;
 
     public static void main(String[] args) {
         SpringApplication.run(HesabgarApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        UserInfo adminInfo = userInfoRepo.getUserInfoByUserName("ADMIN");
+        if(adminInfo == null){
+            adminInfo = new UserInfo();
+            adminInfo.setActive(true);
+            adminInfo.setAdmin(true);
+            adminInfo.setUserName("ADMIN");
+            adminInfo.setPassword("adminadmin");
+            userInfoRepo.save(adminInfo);
+        }
+    }
 }
 
 
